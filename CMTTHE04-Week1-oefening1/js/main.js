c2141903694c83429ac8b8610742fa008b49d6df
TheSas99 <33514432+TheSas99@users.noreply.github.com>
2020-05-11 12:51:56 +0200

    Add files via upload

---
+++ CMTTHE04-Week1-oefening1/js/main.js
@@ -0,0 +1,23 @@
+//
+// vis element
+//
+function showItems(fish, bubble) {
+    let fish = document.createElement("fish")
+document.body.appendChild(fish)
+fish.style.transform = "translate(350px, 400px)"
+fish.style.filter = "hue-rotate(170deg)"
+
+//
+// bubble element
+//
+let bubble = document.createElement("bubble")
+document.body.appendChild(bubble)
+bubble.style.transform = "translate(20px, 100px)"
+
+}
+
+
+var i;
+for (i = 0; i < 100; i++) {
+  return showItems
+}
